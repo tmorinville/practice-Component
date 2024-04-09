0ad62809cfac33d915134f9e0639a8f864e73bdf
Pratap Kotala <pk@yahoo.com>
2024-04-09 17:01:39 -0500

    defined COURSES array of Course[] type

--- src/app/data/course-data.ts
+++ src/app/data/course-data.ts
@@ -1,5 +1,6 @@
+import {Course} from "../model/course";
 
-export const COURSES = [
+export const COURSES:Course[] = [
     {
         id: 1,
         title : "CSci 160: Computer Science - I",
@@ -12,7 +13,7 @@ export const COURSES = [
         id: 2,
         title: "CSci 161: Computer Science - II",
         iconUrl: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
-        longDescription: "Introduction to data structures, algorithms and program complexity",
+        description: "Introduction to data structures, algorithms and program complexity",
         level: '100 level',
         credits: 4
     },
@@ -20,8 +21,8 @@ export const COURSES = [
     {
         id: 3,
         title: 'CSci 213: Modern Software Development ',
-        longDescription: "Introduction to modern software developemnt. C#",
-        iconUrl: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
+        url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
+        description: "Introduction to modern software developemnt. C#",
         level: '200 level',
         credits: 3
     },
@@ -29,31 +30,26 @@ export const COURSES = [
     {
         id: 4,
         title: "CSci 313: Advanced Software Development",
-        iconUrl: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
-        longDescription: "Introduction to modern software frameworks",
+        url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
+        description: "Introduction to modern software frameworks",
         level: '200 level',
         credits: 3
     },
     {
         id: 5,
         title: 'CSci 222: Discrete Math',
-        longDescription: "Discrete Math",
-        iconUrl: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
+        url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
+        description: "Discrete Math",
         level: '200 level',
         credits: 3
     },
     {
         id: 6,
         title: 'CSci 374: Computer Organization',
-        longDescription: 'Computer Architecture and organization',
-        iconUrl: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
+        url: 'https://www.ndsu.edu/themes/ndsu/logo.svg',
+        description: 'Computer Architecture and organization',
         level: '300 level',
         credits: 3
     },
  
 ];
-
-
-export function findCourseById(courseId:number) {
-    return COURSES.find(course => course.id === courseId);
-}
