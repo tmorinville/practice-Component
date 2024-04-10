d0f89e528c994835b5c12bb1ef4bfaa90c93acfa
Taya Morinville <149643813+tmorinville@users.noreply.github.com>
2024-04-10 10:26:09 -0500

    Added course card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
